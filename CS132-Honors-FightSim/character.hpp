b12bbed6fa2b8c67dde26fa465bf25f9c32eee14
Taylor Bisset <taylorbisset@gmail.com>
2023-11-25 12:48:35 -0800

    Add character.hpp

---
+++ CS132-Honors-FightSim/character.hpp
@@ -0,0 +1,67 @@
+/*
+character.hpp
+Character subclass for all character entities in the game.
+This class is the main character class from which all characters will inherit.
+*/
+
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include <iostream>
+#include <string>
+#include "entity.hpp"
+
+class Character : public Entity
+{
+public:
+	Character(const string& name, const string& description,
+		int maxHealth, int currentHealth,
+		int attack, int baseAttack,
+		int defense, int baseDefense,
+		int level, int experience, int coins);
+
+	void setMaxHealth(int newMaxHealth);
+	int getMaxHealth() const;
+
+	void setCurrentHealth(int newCurrentHealth);
+	int getCurrentHealth() const;
+	void modifyCurrentHealth(int healthChange);
+
+	void setAttack(int newAttack);
+	int getAttack() const;
+	void setBaseAttack(int newBaseAttack);
+	int getBaseAttack() const;
+	void modifyAttack(int attackBonus);
+
+	void setDefense(int newDefense);
+	int getDefense() const;
+	void setBaseDefense(int newBaseDefense);
+	int getBaseDefense() const;
+	void modifyDefense(int defenseBonus);
+
+	void setLevel(int newLevel);
+	int getLevel() const;
+
+	void setExperience(int newExperience);
+	int getExperience() const;
+	void modifyExperience(int experiencePoints);
+
+	void setCoins(int newCoins);
+	int getCoins() const;
+	void modifyCoins(int coinChange);
+
+	virtual void displayDetails() const;
+
+private:
+	int maxHealth;
+	int currentHealth;
+	int attack;
+	int baseAttack;
+	int defense;
+	int baseDefense;
+	int level;
+	int experience;
+	int coins;
+};
+
+#endif // CHARACTER_HPP
