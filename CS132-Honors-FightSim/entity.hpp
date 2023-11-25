35d940e8d8b59f252545d4eb5ab9e991d1ac74de
Taylor Bisset <taylorbisset@gmail.com>
2023-11-25 10:29:59 -0800

    Add entity.hpp

---
+++ CS132-Honors-FightSim/entity.hpp
@@ -0,0 +1,46 @@
+/*
+entity.hpp
+Base class for all objects in the game.
+This class is the base entity from which all objects will inherit.
+*/
+
+#ifndef ENTITY_HPP
+#define ENTITY_HPP
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Entity
+{
+public: 
+	Entity(const string& name, const string& description) : 
+		name(name), description(description) {}
+
+	// Name
+	void setName(const string& newName)
+	{
+		name = newName;
+	}
+	string getName() const
+	{
+		return name;
+	}
+
+	// Description
+	void setDescription(const string& newDescription)
+	{
+		description = newDescription;
+	}
+	string getDescription() const
+	{
+		return description;
+	}
+
+private:
+	string name;
+	string description;
+};
+
+#endif // !ENTITY_HPP
